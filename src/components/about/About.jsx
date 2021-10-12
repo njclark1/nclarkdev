127c06408ff26d798db6bbd71716d57e70d8e440
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-10-12 16:58:51 -0400

    mobile formatting an content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,15 +6,22 @@ function About() {
     <>
       <div className="aboutContainer" id="about">
         <p className="aboutText">
-          <br /> I am currently a student.
           <br />
-          <br /> I am currently a student.
+          My primary hobby for a few years has been fitness.
+          <br />
+          <br />
+          I have been going to the gym regularly for around 3 and a half years
+          now.
+          <br />
+          <br />
+          Around a year ago my girlfriend moved in with me and started to join
+          me.
+          <br />
+          <br />
+          It can be a struggle staying consistent with our routine, but it has
+          been incredibly rewarding as well.
         </p>
-        <img
-          className="aboutPicture"
-          src="https://media.istockphoto.com/photos/male-chimpanzee-in-business-clothes-picture-id143921954"
-          alt="about"
-        />
+        <img className="aboutPicture" src="beefy.jpg" alt="beefy" />
       </div>
       <div className="aboutContainer" id="about">
         <p className="aboutText">
@@ -22,30 +29,22 @@ function About() {
           Outside of the gym, my hobbies are primarily related to tech.
           <br />
           <br />
-          Growing up, my father introduced me to computers early in elementary
-          school.
+          My father introduced me to computers early in elementary school and
+          this created a passion for tech that has stuck ever since.
           <br />
           <br />
-          Because of this, I grew an interest in electronics and computers from
-          a young age.
+          I enjoy building and tinkering with PCs and related gear such as
+          keyboards.
           <br />
-          <br />I enjoy building PCs and keyboards.
+          <br />
+          Another thing I inherited from my father is a passion for audio.
+          <br />
+          <br />
+          While he is more focused on speaker setups, I developed an interest in
+          headphones.
         </p>
-        <img
-          className="aboutPicture"
-          src="https://media.istockphoto.com/photos/male-chimpanzee-in-business-clothes-picture-id143921954"
-          alt="about"
-        />
-      </div>
-      <div className="aboutContainer" id="about">
-        <p className="aboutText">
-          <br /> I like headphones.Drown out noise of painful existance.
-        </p>
-        <img
-          className="aboutPicture"
-          src="https://media.istockphoto.com/photos/male-chimpanzee-in-business-clothes-picture-id143921954"
-          alt="about"
-        />
+        <img className="aboutPicture" src="pcs.jpg" alt="computers" />
+        <img className="aboutPicture" src="headphones.jpg" alt="headphones" />
       </div>
     </>
   );
--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -4,15 +4,21 @@ import "./Body.css";
 function Body() {
   return (
     <div className="bodyContainer">
-      <img
-        className="myPicture"
-        src="https://media.istockphoto.com/photos/office-worker-of-the-past-picture-id476924948"
-        alt="me"
-      />
+      <img className="bodyPicture" src="cat.jpg" alt="cat" />
       <p className="bodyText">
         Hi, I'm Nicholas Clark <br />
         <br />I am a computer science student working hard to begin a
         professional career. <br />
+        <br />
+        I am currently working towards expanding my skillset in web development
+        technologies.
+        <br />
+        <br />
+        This site is currently being developed with React and is my first
+        experience with it.
+        <br />
+        <br />I am planning to dip my toes into other libraries and frameworks
+        soon!
       </p>
     </div>
   );
